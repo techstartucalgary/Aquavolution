ed4c0fc93720250bc25f3744387505059f7bc99d
Elgiz Abbasov <lgzabbasov@gmail.com>
2021-10-30 15:29:34 -0600

    Update SpawnFood.cs

--- Aquavolution/Assets/Scripts/SpawnFood.cs
+++ Aquavolution/Assets/Scripts/SpawnFood.cs
@@ -8,30 +8,33 @@ public class SpawnFood : MonoBehaviour
 {
     public GameObject FoodPrefab;
     public Camera Cam;
+    public float Speed = 0.5f;
     private int ScreenWidth;
     private int ScreenHeight;
     private System.Random RNG;
     private Vector3 SpawnPos;
-    // Start is called before the first frame update
+    
     void Start()
     {
         ScreenWidth = Screen.width;
         ScreenHeight = Screen.height;
         RNG = new System.Random();
 
-        InvokeRepeating("randomSpawn", 0.5f, 0.5f);
+        InvokeRepeating("randomSpawn", 0.5f, Speed);
     }
 
-    // Gets x, y integers randomly between 0 and screen width and height in pixels
-    // Then, turns the pixel values into world space units, and instantiates a food prefab at that location
+    // Gets x, y integers randomly between 0 and screen width and height in pixels,
+    // then, turns the pixel values into world space units, and instantiates a food prefab at that location
     void randomSpawn()
     {
         int SpawnX = RNG.Next(0, ScreenWidth);
         int SpawnY = RNG.Next(0, ScreenHeight);
         
-        SpawnPos = Cam.ScreenToWorldPoint(new Vector3(SpawnX, SpawnY, Cam.nearClipPlane));      // Converts pixel values to world-space 
-        
-        GameObject FoodObject = Instantiate(FoodPrefab, SpawnPos, Quaternion.identity);         // Instantiates Prefab
-        FoodObject.SetActive(true);     // Enables prefab
+        // Converts pixel values to world-space
+        SpawnPos = Cam.ScreenToWorldPoint(new Vector3(SpawnX, SpawnY, Cam.nearClipPlane));
+        // Instantiates Prefab
+        GameObject FoodObject = Instantiate(FoodPrefab, SpawnPos, Quaternion.identity);
+        // Enables prefab
+        FoodObject.SetActive(true);
     }
 }
