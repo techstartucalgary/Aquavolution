c764e91ef7169ef02a7ef272af6a49bf55715037
Ben <ben@schmidtfam.us>
2021-11-25 19:55:50 -0700

    #51 Added Ocean Surface, we bob up and down if we try to escape the ocean

--- Aquavolution/Assets/Scripts/Food.cs
+++ Aquavolution/Assets/Scripts/Food.cs
@@ -6,7 +6,6 @@ public class Food : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D Col)        // This method is called whenever a collision is detected, and passes the Component which Food collided with
     {
-        Debug.Log("Collision Detected");
         gameObject.SetActive(false);                // Sets Food to inactive, so it does not show up. Could also delete entirely
     }
 
