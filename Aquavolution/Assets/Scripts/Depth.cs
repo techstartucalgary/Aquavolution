7c1116c78ee1724c59a94e992159bbc3f96e5fb1
Madeline <madeline.mazurek@ucalgary.ca>
2022-01-20 19:07:37 -0700

    changed depth UI to update current version

--- Aquavolution/Assets/Scripts/Depth.cs
+++ Aquavolution/Assets/Scripts/Depth.cs
@@ -4,30 +4,24 @@ using UnityEngine.UI;
 public class Depth : MonoBehaviour {
 
     [SerializeField]
-    private Transform oceanFloorX;
+    private Text DepthXText;
 
     [SerializeField]
-    private Transform oceanFloorY;
+    private Text DepthYText;
+
+    private float DepthX;
+    private float DepthY;
 
     [SerializeField]
-    private Text depthxText;
-
-    [SerializeField]
-    private Text depthyText;
-
-    private float depthx;
-    private float depthy;
+    private GameObject SurfaceOrigin;
 
     private void Update () {
-
-        // Calculate distance value by x and y axis
-        depthx = (transform.position.x - oceanFloorX.transform.position.x);
-
-        depthy = (transform.position.y - oceanFloorY.transform.position.y);
+        // Calculate distance value for x and y from surface origin
+        DepthX = (SurfaceOrigin.transform.position.x - transform.position.x);
+        DepthY = (SurfaceOrigin.transform.position.y - transform.position.y);
 
         // to 2 decimal places "F2"
-        depthxText.text = "x: " + depthx.ToString("F2") + " meters";
-        depthyText.text = "y: " + depthy.ToString("F2") + " meters";
-
+        DepthXText.text = " "; //removed temporarily
+        DepthYText.text = "Depth: " + DepthY.ToString("F2") + " meters";
 	}
 }
