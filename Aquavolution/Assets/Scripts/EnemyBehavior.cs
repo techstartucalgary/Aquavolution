e752f76c5a46a4f0f39799d1a5e5955078fd5960
Elgiz Abbasov <lgzabbasov@gmail.com>
2022-03-02 18:23:05 -0700

    Updated enemy movement & added flip

--- Aquavolution/Assets/Scripts/EnemyBehavior.cs
+++ Aquavolution/Assets/Scripts/EnemyBehavior.cs
@@ -4,39 +4,62 @@ using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {
-    private Transform Transform;
-    private SpriteRenderer SR;
+    // TODO: Why do we need these?
+
+    // private Transform Transform;
+    // private SpriteRenderer SR;
+    // private float StartPosition;
+    // public float PatrolLength;
+    // private Vector3 OldPos = Vector3.zero;
+    
     private Rigidbody2D RB;
-    public float PatrolLength;
-    public int MoveSpeed;
-    private bool Patrolling;
-    private float StartPosition;
-    private Vector3 OldPos = Vector3.zero;
-    private static float SizeChange = 0.2F;
-    private Vector3 ScaleIncrease = new Vector3(SizeChange, SizeChange, 0);
+    public float MoveSpeed;
     public int Size;
+    private bool Patrolling;
+    private static float SizeChange = 0.05F;
+    private Vector3 ScaleIncrease = new Vector3(SizeChange, SizeChange, 0);
+    
+    public Transform SightStart, SightEnd;
+    private bool Collision;
 
     // Start is called before the first frame update
     void Start()
     {
-        SR = GetComponent<SpriteRenderer>();
+        // SR = GetComponent<SpriteRenderer>();
         RB = GetComponent<Rigidbody2D>();
         RunBehavior();
-        StartPosition = RB.position.x;
+        // StartPosition = RB.position.x;
         Size = 1;
     }
 
-    void RunBehavior()
+    void Update() 
     {
-        Patrolling = true;
+        // Tilemaps are set to Layer Solid, which makes Collision True if enemy hits a Solid layer
+        Collision = Physics2D.Linecast(SightStart.position, SightEnd.position, 1 << LayerMask.NameToLayer("Solid"));
+
+        if (Collision) 
+        {
+            FlipEnemy();
+        }
     }
 
     void FixedUpdate()
     {        
         if (Patrolling)
         {
-            RB.MovePosition(new Vector2((Mathf.Sin((2 * Mathf.PI * (Time.time*MoveSpeed/PatrolLength)) - (Mathf.PI / 2)) * (PatrolLength/2) + (PatrolLength/2))+StartPosition,RB.position.y));
+            // RB.MovePosition(new Vector2((Mathf.Sin((2 * Mathf.PI * (Time.time*MoveSpeed/PatrolLength)) - (Mathf.PI / 2)) * (PatrolLength/2) + (PatrolLength/2))+StartPosition,RB.position.y));
+            RB.velocity = new Vector2(transform.localScale.x, 0) * MoveSpeed;
         }
+    }
+
+    public void FlipEnemy() 
+    {
+        transform.localScale = new Vector3(transform.localScale.x == gameObject.transform.localScale.x ? -gameObject.transform.localScale.x : gameObject.transform.localScale.x, gameObject.transform.localScale.y, gameObject.transform.localScale.z);
+    }
+
+    void RunBehavior()
+    {
+        Patrolling = true;
     }
 
     public void GetEaten()
@@ -50,6 +73,10 @@ public class EnemyBehavior : MonoBehaviour
         if (Col.gameObject.tag == "Food") 
         {
             IncreaseFood(1);            
+        }
+        if (Col.gameObject.tag == "Enemy")
+        {
+            FlipEnemy();
         }
     }
 
